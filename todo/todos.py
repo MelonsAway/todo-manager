320c128a2352b85356dc8832ea11e07b0fba3691
Jason Wyatt <stryfecrab22@gmail.com>
2019-02-07 16:32:56 -0500

    updated

--- todo/todos.py
+++ todo/todos.py
@@ -1,2 +1,9 @@
 import * from item.py
 import * from manager.py
+command = input()
+if command == "show"
+    Manager.show()
+elif command == "add"
+    Manager.add()
+elif command == "complete"
+    Manager.complete()
